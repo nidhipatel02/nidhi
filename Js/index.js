7341506e369cab0d8522a0757f41993946f8cbed
nidhi <nidhzzpatel3950@gmail.com>
2024-07-09 09:50:05 +0530

    apicall

--- Js/index.js
+++ Js/index.js
@@ -1,12 +1,43 @@
 document.getElementById("myform").addEventListener("submit", function(event){
     event.preventDefault()
     const payload = {
-        firstname : document.getElementById("firstname"),
-        lastname : document.getElementById("lastname"),
-        number : document.getElementById("number"),
-        email : document.getElementById("email"),
-        number : document.getElementById("rollnumber"),
+        firstname : document.getElementById("firstname").value,
+        lastname : document.getElementById("lastname").value,
+        number : document.getElementById("number").value,
+        email : document.getElementById("email").value,
+        roll_number : document.getElementById("rollnumber").value,
  
     }
-    console.log(payload)
+    // console.log(payload)
+    const apiurl = "http://192.168.194.144:3001/userregister"
+    const option ={
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(payload)
+
+    }
+    fetch(apiurl,option).then(Response =>{
+        if(!Response.ok){
+            console.log("error")
+        }
+        return Response.json()
+        
+    }).then(data =>{
+        console.log(data)
+        userinfo(data)
+    })
 })
+function userinfo(data){
+    const userdatadiv = document.getElementById("userdata")
+    userdatadiv.innerHTML =`
+    <h2>user info</h2>
+    <p>firstname :${data.firstname}</p>
+    <p>lastname :${data.lastname}</p>
+    <p>number :${data.number}</p>
+    <p>email :${data.email}</p>
+    <p>rollnumber :${data.roll_number}</p>
+    
+    `
+}
